422bed9f378886879062400472e401ccc713944e
DeProdigy <alex.hint.ah@gmail.com>
2013-11-20 16:39:31 -0500

    first commit come at me

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewBusApp::Application.config.secret_token = '7fbfd1bf0c0027351f7b4ec73470080e50bc2aaab4f715e368358d3f2a51635eaba537d2a7b3bd15f60c2e02d887716d1e85d912708253a79aed7fcd069843d3'
